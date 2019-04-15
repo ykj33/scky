821ff7c0bbb7e3301887d5a308a34e683a59949a
ykj33 <dnsrjswnd@gmail.com>
2019-04-15 23:27:54 +0900

    한글 깨짐에 대한 utf8 설정

--- scky/post.php
+++ scky/post.php
@@ -1,11 +1,13 @@
-<?php
+﻿<?php
 $servername = "localhost";
 $username = "dnsrjswnd";
 $password = "rjswnd9870!#";
 $dbname = "dnsrjswnd";
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
  
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    mysql_query("set names utf8");
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "INSERT INTO info (name, email, message)
