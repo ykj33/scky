2ccb35d276c200c11bebc54c6516cf3aeecd6006
ykj33 <dnsrjswnd@gmail.com>
2019-04-21 17:26:48 +0900

    DB저장시 한글 깨짐에 대한 수정 : 실패

--- scky/post.php
+++ scky/post.php
@@ -1,16 +1,26 @@
-﻿<?php
+﻿
+<?php
+header('Content-Type: text/html; charset=UTF-8');
 $servername = "localhost";
 $username = "dnsrjswnd";
 $password = "rjswnd9870!#";
 $dbname = "dnsrjswnd";
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 
  
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    mysql_query("set names utf8");
-    // set the PDO error mode to exception
+    mysql_query("SET NAMES euckr"); 
+    mysql_query("set session character_set_connection=utf8;
+
+set session character_set_results=utf8;
+
+set session character_set_client=utf8;");
+
+
+
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO info (name, email, message)
+    $sql = "INSERT INTO info2 (name, email, message)
     VALUES ('$_POST[user_name]', '$_POST[user_email]', '$_POST[user_message]')";
     // use exec() because no results are returned
     $conn->exec($sql);
